3fadcd173cd824c32106a970c5abb147c6a281dc
Daniil <tix.tot@yandex.ru>
2025-09-15 15:15:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿namespace laba_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbs = [1, 2, 3, 4, 5];
+            //int[] numbs2 = { 1, 2, 3, 4, 5 };
+            //foreach(int c in numbs)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            //for (int i = 0; i < numbs.Length; i++)
+            //{
+            //    Console.WriteLine(numbs[i]);
+            //}
+            //dynamic[] text = [1, 2, "Anton"];
+            //object[] text2 = [1, 2, "Anton"];
+
+            //List<string> people = ["Denis", "Anton", "Bogdan"];
+            //List<string> people2 = new List<string> { "Denis", "Anton", "Bogdan" };
+            //people.Add("Dima");
+            //people.Insert(1, "Kirill");
+            //people.Remove("Bogdan");
+            //people.RemoveAt(1);
+            //people.Clear();
+            //Console.WriteLine(people.Count);
+            //int[] numbers = [-1, 34, 235, 123, 53, 25, 236, -23, -52, -612, 234234];
+            //int result = 0;
+            //foreach (int number in numbers)
+            //{
+            //    if (number > 0)
+            //        result++;
+            //}
+            //Console.WriteLine($"Число элементов больше нуля: {result}");
+            //for(int i = 0; i < numbers.Length;)
+            //{
+            //    if (i > 0)
+            //    {
+            //        i++;
+            //    }
+            //}
+            //int k = 0;
+            //while (k < numbers.Length)
+            //{
+            //    if (k > 0) {
+            //        k++;
+            //    }
+
+            //}
+            //int[] numbers = [-1, 34, 235, 123, 53, 25, 236, -23, -52, -612, 234234];
+            //int n = numbers.Length;
+            //int k = n / 2;
+            //int temp;
+            //for(int i =0;i < k;i++)
+            //{
+            //    temp = numbers[i];
+            //    numbers[i] = numbers[n - i - 1];
+            //    numbers[n - i - 1] = temp;
+            //}
+            //foreach(int i in numbers)
+            //{
+            //    Console.WriteLine($"{i}\t");
+            //}
+            //int[] nums = [54, 2, 523, -5, 235, 235, 2, 34, 25, -22134, 2];
+            //int temp;
+            //for(int i = 0;i < nums.Length - 1;i++)
+            //{
+            //    for(int j = i+1; j < nums.Length; j++)
+            //    {
+            //        if (nums[i] > nums[j])
+            //        {
+            //            temp = nums[i];
+            //            nums[i] = nums[j];
+            //            nums[j] = temp;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Вывод отсортированого массива");
+            //for(int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine(nums[i]);
+            //}
+
+
+        }
+    }
+}
